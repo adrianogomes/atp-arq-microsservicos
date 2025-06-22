11a5b22a5e75e2fe6e824a2fcbf1f9b81db8b8de
Adriano Gebert Gomes <124310443+adriano-gomes-dev@users.noreply.github.com>
2025-06-22 13:56:18 -0300

    adicionado HTTPException

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from tinydb import TinyDB, Query
 from modelo import Telefone, CategoriaContato
 
 app = FastAPI()
 
-db = TinyDB('database.json')
-Contato = Query()
+db  = TinyDB('database.json')
+ContatoQuery = Query()
 
 # Inclusão de um contato
 @app.post('/adicionar')
@@ -22,14 +22,24 @@ def adicionar(nome: str, telefones : list[Telefone], categoria : CategoriaContat
         "categoria" : categoria
     })
 
-    return {"status" : "done"}
+    return {"message" : "Contato criado com sucesso"}
 
 # Consulta de um contato
 @app.get('/consultar/{id_contato}')
 def consultar(id_contato : int):
-    return db.search(Contato.id_contato == id_contato)
+    contato = db.search(ContatoQuery.id_contato == id_contato)
+
+    if contato == []:
+        raise HTTPException(status_code=400, detail="Contato não encontrado")
+    
+    return contato
 
 # Listar contatos cadastrados
 @app.get('/listar')
 def listar():
-    return db.all()
+    contatos = db.all()
+
+    if contatos == []:
+        raise HTTPException(status_code=400, detail="Sem contatos cadastrados")
+    
+    return contatos
